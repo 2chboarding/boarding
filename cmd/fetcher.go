1afd740be3480190525649a3c9e5f9a918c688f3
anon <2boarding@rambler.ru>
2019-08-21 15:14:35 +0500

    Dummy JSON fetcher

--- cmd/fetcher.go
+++ cmd/fetcher.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
 )
 
 func getJSON(url string) ([]byte, error) {
@@ -27,39 +28,43 @@ func getJSON(url string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func getJSONStub(filename string) ([]byte, error) {
-	log.Printf("Loading %v", filename)
+func getJSONStub(url string) ([]byte, error) {
+	//log.Printf("Fake loading %v", url)
 
+	var filename string
+
+	if strings.Contains(url, "get_board") {
+		filename = "data/boards.json"
+	} else if strings.Contains(url, "index.json") {
+		filename = "data/board_index.json"
+	} else if strings.Contains(url, "/res/") {
+		filename = "data/full_thread.json"
+	}
+
+	if filename == "" {
+		return []byte{}, errors.New("Invalid filename")
+	}
 	return ioutil.ReadFile(filename)
 }
+
+//var getterFunc = getJSONStub
+var getterFunc = getJSON
 
 // GetBoardsCatalog загружает данные с сайта
 func GetBoardsCatalog() ([]byte, error) {
 	uri := fmt.Sprintf("https://2ch.hk/makaba/mobile.fcgi?task=get_boards")
-	return getJSON(uri)
-}
-
-// GetBoardsCatalogStub читает данные из файла
-func GetBoardsCatalogStub() ([]byte, error) {
-	uri := fmt.Sprintf("../../data/boards.json")
-	return getJSONStub(uri)
+	return getterFunc(uri)
 }
 
 // GetThreads load json from given boards containing list of threads (first page)
 func GetThreads(boardID string) ([]byte, error) {
 	url := fmt.Sprintf("https://2ch.hk/%v/index.json", boardID)
 	//url := fmt.Sprintf("https://2ch.hk/%v/catalog.json", boardID)
-	return getJSON(url)
-}
-
-// GetThreadsStub читает данные из файла
-func GetThreadsStub(boardID string) ([]byte, error) {
-	uri := "../../data/board_index.json"
-	return getJSONStub(uri)
+	return getterFunc(url)
 }
 
 // GetThread получает полный тред с номером num с доски boardID
 func GetThread(boardID string, num PostID) ([]byte, error) {
 	uri := fmt.Sprintf("https://2ch.hk/%v/res/%v.json", boardID, num)
-	return getJSON(uri)
+	return getterFunc(uri)
 }
